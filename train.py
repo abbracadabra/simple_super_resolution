24d89453f4f34f497fddffa78ddf7bf35191398e
abbracadabra <gyl_mail@qq.com>
2019-02-19 22:50:58 +0800

    a

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ hr = tf.placeholder(dtype=tf.float32,shape=[None,200,200,3])
 dnet = discriminatenet(sr)
 prob = dnet.output
 flag = tf.placeholder(dtype=tf.float32,shape=[None])#0:fake 1:real
-dnet_loss = flag*(1.-prob)**2 + (1-flag)*(prob-0.)**2
+dnet_loss = tf.reduce_mean(flag*(1-prob)**2 + (1-flag)*(prob-0)**2)
 gan_loss = tf.reduce_mean((1-prob)**2)
 pixel_loss = tf.reduce_mean((sr-hr)**2)
 gnet_loss = pixel_loss + gan_loss
@@ -29,7 +29,7 @@ class Trainer:
         self.minpos = -1
     def train(self):
         for i in range(config.epochs):
-            for hrs, lrs in gendata():
+            for i,(hrs, lrs) in enumerate(gendata()):
                 _, self.glossval, fakeimgs = self.sess.run([gnetops, gnet_loss, sr], feed_dict={hr: hrs, lr: lrs})
                 mixims = np.concatenate((hrs, fakeimgs))
                 flgs = np.array([1] * len(hrs) + [0] * len(fakeimgs))
@@ -38,23 +38,24 @@ class Trainer:
                 self.preview()
     def savebest(self):
         print(self.glossval)
-        if len(self.rec) - self.minpos > 50:
+        if len(self.rec) - self.minpos > 100:
             raise Exception("performance not improving")
         self.rec.append(self.glossval)
         if self.glossval < self.minloss:
             self.minloss = self.glossval
             self.minpos = len(self.rec) - 1
-            if len(self.rec) % 20:
+            if len(self.rec) % 5:
                 self.saver.save(self.sess, config.modelpath)
     def preview(self):
-        flist = os.listdir(testdir)
-        fn = np.random.choice(flist)
-        fp = os.path.join(testdir,fn)
-        im = Image.open(fp)
-        im = np.array(im) / 255.
-        fakeim = self.sess.run(sr,feed_dict={lr:np.array([im])})
-        im = Image.fromarray(np.squeeze(np.uint8(fakeim*255)))
-        im.save(os.path.join(outtestdir,fn))
+        if len(self.rec)%10==0:
+            flist = os.listdir(testdir)
+            fn = np.random.choice(flist)
+            fp = os.path.join(testdir, fn)
+            im = Image.open(fp)
+            im = np.array(im) / 255.
+            fakeim = self.sess.run(sr, feed_dict={lr: np.array([im])})
+            im = Image.fromarray(np.squeeze(np.uint8(fakeim * 255)))
+            im.save(os.path.join(outtestdir, fn))
 
 if __name__ == '__main__':
     Trainer().train()
