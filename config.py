d493cf6e06247847d015b8538385bde2c3c165e0
abbracadabra <gyl_mail@qq.com>
2019-02-19 18:02:40 +0800

    g

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+import os
+
+basedir = os.path.dirname(os.path.realpath(__file__))
+
 epochs = 3
 
 traindir = r'D:\Users\yl_gong\Desktop\dl\voc\VOCtest_06-Nov-2007\VOCdevkit\VOC2007\JPEGImages'
 
-modelpath = r"D:\githubrepo\simple_super_resolution\model\srgan"
+modelpath = os.path.join(basedir,"srgan")
+testdir = os.path.join(basedir,"testimages")
+outtestimages = os.path.join(basedir,"outtestimages")
+
+
--- train.py
+++ train.py
@@ -18,31 +18,35 @@ gnet_loss = pixel_loss + gan_loss
 gnetops = tf.train.AdamOptimizer(learning_rate=0.001).minimize(gnet_loss,tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope='gnet'))
 dnetops = tf.train.AdamOptimizer(learning_rate=0.001).minimize(dnet_loss,tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope='dnet'))
 
-def train():
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-    saver = tf.train.Saver()
-    rec = []
-    minloss = 999999999
-    minpos = -1
-    for i in range(config.epochs):
-        for hrs, lrs in gendata():
-            _,glossval,fakeimgs = sess.run([gnetops,gnet_loss,sr],feed_dict={hr:hrs,lr:lrs})
-            mixims = np.concatenate((hrs,fakeimgs))
-            flgs = np.array([1]*len(hrs) + [0]*len(fakeimgs))
-            _,dlossval = sess.run([dnetops,dnet_loss],feed_dict={sr:mixims,flag:flgs})
-            #callbacks
-            if len(rec)-minpos>50:
-                return
-            rec.append(glossval)
-            if glossval< minloss:
-                minloss = glossval
-                minpos = len(rec)-1
-                if len(rec)%20:
-                    saver.save(sess,config.modelpath)
+class Trainer:
+    def __init__(self):
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
+        self.rec = []
+        self.minloss = 999999999
+        self.minpos = -1
+    def train(self):
+        for i in range(config.epochs):
+            for hrs, lrs in gendata():
+                _, self.glossval, fakeimgs = self.sess.run([gnetops, gnet_loss, sr], feed_dict={hr: hrs, lr: lrs})
+                mixims = np.concatenate((hrs, fakeimgs))
+                flgs = np.array([1] * len(hrs) + [0] * len(fakeimgs))
+                _, dlossval = self.sess.run([dnetops, dnet_loss], feed_dict={sr: mixims, flag: flgs})
+                self.callback()
+    def callback(self):
+        if len(self.rec) - self.minpos > 50:
+            raise Exception("performance not improving")
+        self.rec.append(self.glossval)
+        if self.glossval < self.minloss:
+            self.minloss = self.glossval
+            self.minpos = len(self.rec) - 1
+            if len(self.rec) % 20:
+                self.saver.save(self.sess, config.modelpath)
+
 
 if __name__ == '__main__':
-    train()
+    Trainer().train()
 
 
 
