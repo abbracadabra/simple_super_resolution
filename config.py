4ce25434778c3061fc7db857492f80cbf10ad800
Steven Lee <gyl_mail@qq.com>
2019-07-04 15:56:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 epochs = 3
-traindir = r'D:\Users\yl_gong\Desktop\dl\voc\VOCtest_06-Nov-2007\VOCdevkit\VOC2007\JPEGImages'
+traindir = r'D:\Users\xxx\Desktop\dl\voc\VOCtest_06-Nov-2007\VOCdevkit\VOC2007\JPEGImages'
 modelpath = os.path.join(basedir,"mdl","srgan")
 testdir = os.path.join(basedir,"testimages")
 outtestdir = os.path.join(basedir,"outtestimages")
